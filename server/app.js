28cef774f28f22d4f77b1f28f3bd10e86da37e95
Rezazi Abdelbasset <basset.rezazi26@gmail.com>
2023-03-04 11:26:58 +0100

    integrate with front

--- server/app.js
+++ server/app.js
@@ -1,30 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const authRouter = require('./routes/auth')
-const userRouter = require('./routes/user')
-const productRouter = require('./routes/product')
-const investRouter = require('./routes/invest')
-const stripeRouter = require('./routes/stripe')
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
+const productRouter = require("./routes/product");
+const investRouter = require("./routes/invest");
+const stripeRouter = require("./routes/stripe");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/auth',authRouter)
-app.use('/user',userRouter)
-app.use('/product',productRouter)
-app.use('/invest',investRouter)
-app.use('/',stripeRouter)
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+app.use("/invest", investRouter);
+app.use("/", stripeRouter);
 
-app.listen(5000,()=>{
-    console.log('the server is starting in port 5000,')
-})
+app.listen(8000, () => {
+  console.log("the server is starting in port 8000");
+});
 
-mongoose.connect('mongodb://localhost:27017/mc-hack')
-.then(()=>console.log('Connecting MongoDB'))
-.catch((err)=>console.log(err))
-
+mongoose
+  .connect("mongodb://localhost:27017/mc-hack")
+  .then(() => console.log("Connecting MongoDB"))
+  .catch((err) => console.log(err));
